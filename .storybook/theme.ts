967dd9e4b803ccb8fc1f20a4e7dba418c41e375d
joaopmo <joao.oliveira@dcc.ufmg.br>
2023-07-16 12:50:32 -0300

    Update readme

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'dark',
   brandTitle: 'React Paper',
-  brandUrl: 'https://example.com',
+  brandUrl: 'https://main--64b35eb2b079785924e82a2f.chromatic.com/',
   brandImage: 'logo-white.svg',
   brandTarget: '_self',
 });
